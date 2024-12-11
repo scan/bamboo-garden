55b2c83c4e6f5b774aed7fc6e9dacd400449bf1f
Tom Streller <tom@streller.me>
2024-12-11 10:15:48 +0000

    Set up dev container

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+#[derive(thiserror::Error, Debug)]
+enum SetupError {
+    #[error("failed to setup dotenv")]
+    DotEnvError(#[from] dotenv::Error),
+    #[error("failed to setup tracing")]
+    TracingError(#[from] tracing::subscriber::SetGlobalDefaultError),
+}
+
 #[tokio::main]
-async fn main() -> Result<(), ()> {
-    dotenv::dotenv().expect("failed to setup dotenv");
-    pretty_env_logger::init();
+async fn main() -> Result<(), SetupError> {
+    dotenv::dotenv()?;
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber)?;
 
     println!("Hello, world!");
     Ok(())
